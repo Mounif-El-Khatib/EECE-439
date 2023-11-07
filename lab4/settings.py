4cd80e7a2a90b3148e94696bab8ddf5bd39765a6
mounif <mounifelkhatib@hotmail.com>
2023-11-07 16:33:52 +0200

    s

--- lab4/settings.py
+++ lab4/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0=d6)-=x6j@35wh_q=im9s$k1g+8nbyu574+i+(2*bkg*0tu=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     '*', 'CLwebapp1.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://clwebapp1.azurewebsites.net']
